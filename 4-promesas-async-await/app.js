8665353422cbb6e35dd70dfd0d0340cf6cd229a2
LAGS710 <luisgonzales2_hotmail.com>
2025-06-04 20:27:36 -0600

    Entregable 4 Promesas / Async y Await

---
+++ 4-promesas-async-await/app.js
@@ -0,0 +1,46 @@
+// Aquí tienes un código incompleto para tomar como base. Cada función está definida, pero los pasos cruciales aún no están implementados.
+
+// Simulando una base de datos de mesas
+const mesasDisponibles = 5;  // Número de mesas disponibles para reservar
+
+// Función que simula la verificación de disponibilidad de mesas
+function verificarDisponibilidad(mesasSolicitadas) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      // Completa la lógica aquí: Si hay suficientes mesas disponibles, resuelve la promesa, 
+      if (mesasSolicitadas <= mesasDisponibles){
+        resolve("Hola");
+      } else{
+        // de lo contrario, recházala con un mensaje adecuado.
+        reject("No hay mesas suficientes para su reservación");
+      }
+    }, 2000);  // Simula un retraso en la verificación (2 segundos)
+  });
+}
+
+// Función que simula el envío de un correo de confirmación
+function enviarConfirmacionReserva(nombreCliente) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      // para simular si el correo se envió correctamente o si ocurrió un error.
+      if(Math.floor(Math.random()*10) > 5){
+        resolve(nombreCliente, mesasSolicitadas)
+      } else {
+        reject("La reservación falló, intente mas tarde.")
+      }
+    }, 1500);  // Simula el envío de un correo (1.5 segundos)
+  });
+}
+
+// Función principal para manejar una reserva
+async function hacerReserva(nombreCliente, mesasSolicitadas) {
+  try {
+    console.log("Verificando disponibilidad de mesas...");
+    const disponibilidad = await verificarDisponibilidad(mesasSolicitadas);  // Llama a la función de verificación
+  } catch (error) {
+    console.log("Error:", error);  // Maneja los errores en la promesa
+  }
+}
+
+// Llamada de prueba
+hacerReserva("Juan Pérez", 3);  // Intenta hacer una reserva para 3 persona
