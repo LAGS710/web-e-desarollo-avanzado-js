89dfdb4795f6d90a32ff85c1c119b5356dace920
LAGS710 <luisgonzales2_hotmail.com>
2025-06-05 15:24:41 -0600

    Entregable 7 y 8

---
+++ 7.1-mi-exploracion-espacial/planetas.js
@@ -0,0 +1,31 @@
+
+const planetas = [
+    {
+      nombre: "Titán",
+      descripcion: "La luna más grande de Saturno, con lagos de metano.",
+      descubiertoEn: "1655"
+    },
+    {
+      nombre: "Próxima Centauri b",
+      descripcion: "Un exoplaneta rocoso en la zona habitable de su estrella.",
+      descubiertoEn: "2016"
+    },
+    {
+      nombre: "COROT-7b",
+      descripcion: "Conocido como el planeta del infierno, COROT-7b es un planeta rocoso que orbita muy cerca de su estrella, con un periodo orbital de 20 horas. Su lado expuesto alcanza temperaturas de hasta 2,600 °C, mientras que el lado opuesto es extremadamente frío.",
+      descubiertoEn: "2009"
+    },
+    {
+      nombre: "HAT-P-12b",
+      descripcion: "Este exoplaneta, ubicado a aproximadamente 464 años luz de la Tierra, es un Júpiter caliente debido a su proximidad a su estrella. Su órbita de solo 0.0384 UA le permite completar un ciclo en poco tiempo, resultando en temperaturas extremadamente altas.",
+      descubiertoEn: "2009"
+    },
+    {
+      nombre: "4. 55 Cancri e (Janssen)",
+      descripcion: "Este planeta extrasolar posee una masa similar a la de Neptuno y orbita la estrella gemela solar Copernico, ubicada a 41 años luz de la Tierra. Es el más cercano a su estrella dentro de su sistema planetario conocido y requiere menos de tres días para completar su órbita.",
+      descubiertoEn: "2004"
+    }
+    // ¡Añade más planetas!
+  ];
+  
+  module.exports = planetas;
